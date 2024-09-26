8a014b2b7df76d19e90d3469fe82f5c518204257
seetal jhamut <seetal.jhamut@intechnica.co.uk>
2024-09-26 16:58:03 +0100

    added mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: "directory"
+  }),
 });
