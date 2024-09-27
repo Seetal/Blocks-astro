4231b2165cdc684bc75d3e3c45e2826dd027b452
seetal jhamut <seetal.jhamut@intechnica.co.uk>
2024-09-27 11:10:01 +0100

    endpoints test 2

--- src/pages/getTopTen.json.js
+++ src/pages/getTopTen.json.js
@@ -1,6 +1,20 @@
+import { db } from "../db";
+import { topten } from "../db/schema";
+
 export const GET = async ({ request }) => {
-  return new Response(JSON.stringify({message: 'Hello'}), {
-    status: 200,
-    headers: { 'content-type': 'application/json'},
-  })
+  try {
+    const response = await db.select().from(topten);
+    return new Response(
+      JSON.stringify({
+        ok: true,
+        data: response
+      })
+    )
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        ok: false
+      })
+    )
+  }
 }
--- src/pages/test.json.js
+++ src/pages/test.json.js
@@ -1,5 +1,7 @@
 export const GET = async ({ request }) => {
-  return new Response(JSON.stringify({message: 'Hello'}), {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await response.json();
+  return new Response(JSON.stringify({message: data}), {
     status: 200,
     headers: { 'content-type': 'application/json'},
   })
