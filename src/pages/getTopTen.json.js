f463e78e20f3709f1d73af6caaed0460aaeba835
seetal jhamut <seetal.jhamut@intechnica.co.uk>
2024-09-27 10:37:56 +0100

    endpoints test

--- src/pages/getTopTen.json.js
+++ src/pages/getTopTen.json.js
@@ -2,19 +2,23 @@ import { db } from "../db";
 import { topten } from "../db/schema";
 
 export const GET = async ({ request }) => {
-  try {
-    const response = await db.select().from(topten);
-    return new Response(
-      JSON.stringify({
-        ok: true,
-        data: response
-      })
-    )
-  } catch (error) {
-    return new Response(
-      JSON.stringify({
-        ok: false
-      })
-    )
-  }
+  return new Response(JSON.stringify({message: 'Hello'}), {
+    status: 200,
+    headers: { 'content-type': 'application/json'},
+  })
+  // try {
+  //   const response = await db.select().from(topten);
+  //   return new Response(
+  //     JSON.stringify({
+  //       ok: true,
+  //       data: response
+  //     })
+  //   )
+  // } catch (error) {
+  //   return new Response(
+  //     JSON.stringify({
+  //       ok: false
+  //     })
+  //   )
+  // }
 }
