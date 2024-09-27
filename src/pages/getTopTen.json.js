ae7e59ab5fca7069ba3211c79cf84d7c921315b1
seetal jhamut <seetal.jhamut@intechnica.co.uk>
2024-09-27 11:54:13 +0100

    endpoints test 4

--- src/pages/getTopTen.json.js
+++ src/pages/getTopTen.json.js
@@ -5,13 +5,11 @@ export const GET = async ({ request }) => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
     const data = await response.json();
+    return new Response(JSON.stringify({message: data}), {
+      status: 200,
+      headers: { 'content-type': 'application/json'},
+    })
     // const response = await db.select().from(topten);
-    return new Response(
-      JSON.stringify({
-        ok: true,
-        data: data
-      })
-    )
   } catch (error) {
     return new Response(
       JSON.stringify({
